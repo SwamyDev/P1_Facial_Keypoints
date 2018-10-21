c778e695e175678ed300f2ad7e3d6a4f556cd034
Bernhard <b.raml@gmx.at>
2018-10-21 20:34:37 +0200

    Add vertical flip data augmentation
    To improve generalization of the model a random vertical flip data
    transformation has been implemented. This improved the validation RMSE.

--- data_load.py
+++ data_load.py
@@ -1,11 +1,11 @@
-import glob
 import os
-import torch
-from torch.utils.data import Dataset, DataLoader
-import numpy as np
-import matplotlib.image as mpimg
-import pandas as pd
+
 import cv2
+import matplotlib.image as mpimg
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
 
 
 class FacialKeypointsDataset(Dataset):
@@ -28,14 +28,14 @@ class FacialKeypointsDataset(Dataset):
 
     def __getitem__(self, idx):
         image_name = os.path.join(self.root_dir,
-                                self.key_pts_frame.iloc[idx, 0])
-        
+                                  self.key_pts_frame.iloc[idx, 0])
+
         image = mpimg.imread(image_name)
-        
+
         # if image has an alpha color channel, get rid of it
-        if(image.shape[2] == 4):
-            image = image[:,:,0:3]
-        
+        if image.shape[2] == 4:
+            image = image[:, :, 0:3]
+
         key_pts = self.key_pts_frame.iloc[idx, 1:].as_matrix()
         key_pts = key_pts.astype('float').reshape(-1, 2)
         sample = {'image': image, 'keypoints': key_pts}
@@ -44,31 +44,28 @@ class FacialKeypointsDataset(Dataset):
             sample = self.transform(sample)
 
         return sample
-    
 
-    
-# tranforms
+
+# transforms
 
 class Normalize(object):
-    """Convert a color image to grayscale and normalize the color range to [0,1]."""        
+    """Convert a color image to grayscale and normalize the color range to [0,1]."""
 
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
-        
+
         image_copy = np.copy(image)
         key_pts_copy = np.copy(key_pts)
 
         # convert image to grayscale
         image_copy = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        
+
         # scale color range from [0, 255] to [0, 1]
-        image_copy=  image_copy/255.0
-            
-        
+        image_copy = image_copy / 255.0
+
         # scale keypoints to be centered around 0 with a range of [-1, 1]
         # mean = 100, sqrt = 50, so, pts should be (pts - 100)/50
-        key_pts_copy = (key_pts_copy - 100)/50.0
-
+        key_pts_copy = (key_pts_copy - 100) / 50.0
 
         return {'image': image_copy, 'keypoints': key_pts_copy}
 
@@ -101,7 +98,7 @@ class Rescale(object):
         new_h, new_w = int(new_h), int(new_w)
 
         img = cv2.resize(image, (new_w, new_h))
-        
+
         # scale the pts, too
         key_pts = key_pts * [new_w / w, new_h / h]
 
@@ -134,9 +131,35 @@ class RandomCrop(object):
         left = np.random.randint(0, w - new_w)
 
         image = image[top: top + new_h,
-                      left: left + new_w]
+                left: left + new_w]
 
         key_pts = key_pts - [left, top]
+
+        return {'image': image, 'keypoints': key_pts}
+
+
+class RandomVFlip(object):
+    """Randomly flip the image vertically with the specified probability"""
+
+    def __init__(self, probability):
+        self.probability = probability
+        # remap the mirrored key points to the correct targets
+        self.remapping = np.array([16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,  # cheeks
+                                   26, 25, 24, 23, 22, 21, 20, 19, 18, 17,  # brows
+                                   27, 28, 29, 30,  # nose
+                                   35, 34, 33, 32, 31,  # nostrils
+                                   45, 44, 43, 42, 47, 46, 39, 38, 37, 36, 40, 41,  # eyes
+                                   54, 53, 52, 51, 50, 49, 48,  # upper lip
+                                   59, 58, 57, 56, 55,  # lower lip
+                                   64, 63, 62, 61, 60, 67, 66, 65])  # mouth
+
+    def __call__(self, sample):
+        image, key_pts = sample['image'], sample['keypoints']
+        _, w = image.shape[:2]
+
+        if np.random.rand() < self.probability:
+            image = cv2.flip(image, 1)
+            key_pts = np.array([[w - x, y] for x, y in key_pts])[self.remapping]
 
         return {'image': image, 'keypoints': key_pts}
 
@@ -146,16 +169,16 @@ class ToTensor(object):
 
     def __call__(self, sample):
         image, key_pts = sample['image'], sample['keypoints']
-         
+
         # if image has no grayscale color channel, add one
-        if(len(image.shape) == 2):
+        if (len(image.shape) == 2):
             # add that third color dim
             image = image.reshape(image.shape[0], image.shape[1], 1)
-            
+
         # swap color axis because
         # numpy image: H x W x C
         # torch image: C X H X W
         image = image.transpose((2, 0, 1))
-        
+
         return {'image': torch.from_numpy(image),
                 'keypoints': torch.from_numpy(key_pts)}
